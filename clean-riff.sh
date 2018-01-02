68e2e516a6e8d1dad70feff642348d02a63d8178
Brian McClain <brianmmcclain@gmail.com>
2018-01-02 11:57:43 -0500

    Add environment setup output

--- clean-riff.sh
+++ clean-riff.sh
@@ -2,3 +2,9 @@
 
 helm delete demo --purge
 helm install riffrepo/riff --name demo --set httpGateway.service.type=NodePort
+
+echo -e "\n\nTo configure your terminal, run...\n
+eval \$(minikube docker-env)
+export GATEWAY=\`minikube service --url demo-riff-http-gateway\`
+export HEADER=\"Content-Type: text/plain\"
+"
