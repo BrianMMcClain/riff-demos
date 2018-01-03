4b87058d01997ca5fe17eba35d57b82546761307
Brian McClain <brianmmcclain@gmail.com>
2018-01-03 14:02:48 -0500

    Clean up functions, topics and pods prior to reinstalling riff

--- clean-riff.sh
+++ clean-riff.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+# Delete functions, topics and any running function pods
+FUNCS=`kubectl get functions -o json | jq -r '.items | sort_by(.spec.nodeName)[] | [.metadata.name] | @tsv'`
+for FUNC in $FUNCS
+do
+    kubectl delete function $FUNC 2> /dev/null
+    kubectl delete topic $FUNC 2> /dev/null
+    PODS=`kubectl get pods -o json | jq -r '.items | sort_by(.spec.nodeName)[] | [.metadata.name] | @tsv' | grep $FUNC`
+    sleep 3 # Wait for the function to be deleted
+    for POD in $PODS
+    do
+        kubectl delete pod $POD 2> /dev/null
+    done
+done
+
+# Delete Riff install
 helm delete demo --purge
+
+# Reinstall Riff
 helm install riffrepo/riff --name demo --set httpGateway.service.type=NodePort
 
 echo -e "\n\nTo configure your terminal, run...\n
