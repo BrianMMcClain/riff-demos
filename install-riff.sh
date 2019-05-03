af1c6cac7f9d6062a7c999c6b80a85b2a40c5d39
Brian McClain <brianmmcclain@gmail.com>
2019-05-03 16:47:35 -0400

    Update to install riff via the riff cli

--- install-riff.sh
+++ install-riff.sh
@@ -2,82 +2,55 @@
 
 set -e
 
+if [ $# -lt 1 ]
+then
+    echo "USAGE: ./install-riff DOCKER_ID"
+    exit 1
+fi
+DOCKER_ID=$1
+
 # Install HomeBrew if it doesn't exist
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
+    echo "Installing Homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
+    echo "Running 'brew update'..."
     brew update
 fi
 
-# Ensure wget is installed
-brew install wget
-
 # Install minikube
-echo "Installing Minikube CLI..."
-brew cask install minikube
-
-# Install Helm
-echo "Installing Helm CLI..."
-brew install kubernetes-helm
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Installing Minikube CLI..."
+    brew cask install minikube
+fi
 
 # Start Minikube
 echo "Starting Minikube..."
-minikube start --memory=4096 --bootstrapper=kubeadm
+minikube start --memory=4096 --cpus=4 \
+--kubernetes-version=v1.14.0 \
+--vm-driver=hyperkit \
+--bootstrapper=kubeadm \
+--extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
 eval $(minikube docker-env)
 
-# Setup Helm
-echo "Setting up Helm..."
-kubectl -n kube-system create serviceaccount tiller
-kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account=tiller
-
-helm repo add projectriff https://riff-charts.storage.googleapis.com
-helm repo update
-
-# Wait for Tiller to start
-echo "Waiting for Tiller to start..."
-TILLER_WAIT=0
-until helm version 1> /dev/null 2> /dev/null
-do
-  sleep 1
-  TILLER_WAIT=$((TILLER_WAIT+1))
-  if [ $TILLER_WAIT -eq 30 ]
-  then
-      echo "Could not connect to Tiller, exiting!"
-      exit -1
-  fi
-done
-
-echo "Tiller ready!"
-
-
-# Install Kafka and Riff
-echo "Installing Kafka and Riff..."
-helm install projectriff/riff --version 0.0.7 --name projectriff --namespace riff-system --set kafka.create=true --set httpGateway.service.type=NodePort --wait
-
-# Install the Riff CLI
-echo "Installing Riff CLI..."
-# go get -u github.com/projectriff/riff
-# The current dev build has some inconsistancies, so pull
-# a static build
-curl -Lo riff-darwin-amd64.tgz https://github.com/projectriff/riff/releases/download/v0.0.7/riff-darwin-amd64.tgz
-tar xvzf riff-darwin-amd64.tgz
-mv riff /usr/local/bin/
-
-RIFF_PATH=`which riff`
-if [ "$RIFF_PATH" != "/usr/local/bin/riff" ]
-then
-    echo "Current Riff path is not /usr/local/bin/riff"
-    echo "Patching current Riff location with correct build"
-    cp /usr/local/bin/riff $RIFF_PATH
+# Install the riff CLI
+which -s riff
+if [[ $? != 0 ]] ; then
+    echo "Installing riff CLI..."
+    brew install riff
 fi
+
+# Install riff
+echo "Installing riff..."
+riff system install --node-port
 
 # Wait for Riff to start
 echo "Waiting for Riff to start..."
 RIFF_WAIT=0
-until kubectl get po,deploy --namespace riff-system | grep Running | wc -l | grep 5 1> /dev/null 2> /dev/null
+until kubectl get po,deploy --all-namespaces | grep Running | wc -l | grep 5 1> /dev/null 2> /dev/null
 do
   sleep 5
   RIFF_WAIT=$((RIFF_WAIT+1))
@@ -88,18 +61,6 @@ do
   fi
 done
 
+riff namespace init default --docker-hub $DOCKER_ID
+
 echo "Riff ready!"
-
-# Install Riff invokers
-echo "Installing Riff invokers..."
-riff invokers apply -f https://github.com/projectriff/command-function-invoker/raw/v0.0.6/command-invoker.yaml
-riff invokers apply -f https://github.com/projectriff/go-function-invoker/raw/v0.0.3/go-invoker.yaml
-riff invokers apply -f https://github.com/projectriff/java-function-invoker/raw/v0.0.7/java-invoker.yaml
-riff invokers apply -f https://github.com/projectriff/node-function-invoker/raw/v0.0.8/node-invoker.yaml
-riff invokers apply -f https://github.com/projectriff/python3-function-invoker/raw/v0.0.6/python3-invoker.yaml
-
-echo -e "\n\nTo configure your terminal, run...\n
-eval \$(minikube docker-env)
-export GATEWAY=\`minikube service --url projectriff-riff-http-gateway --namespace riff-system\`
-export HEADER=\"Content-Type: text/plain\"
-"
